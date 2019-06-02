08183fea19fb6a54cf27a23e506de9f3a3e97fc4
Francososa <franky.sosa@gmail.com>
2019-06-02 16:13:35 -0600

    Add Applicative examples

---
+++ src/Applicative.hs
@@ -0,0 +1,61 @@
+module Applicative where
+
+import Data.List (sortBy)
+import Data.Function (on)
+import Numeric.Natural
+
+
+data Option a = Some a | None deriving (Eq, Show)
+
+instance Functor Option where
+  fmap f None = None
+  fmap f (Some x) = Some (f x)
+  
+
+
+data User = User { name :: String
+                 , surname :: String
+                 , age :: Natural
+                 , beta :: Bool
+                 , admin :: Bool }
+  deriving (Eq, Show)
+
+
+userList :: [User]
+userList = [
+  User "Julie" "Moronuki" 74 False True,
+  User "Chris" "Martin" 25 False True,
+  User "Alonzo" "Church" 100 True False,
+  User "Alan" "Turing" 99 True False,
+  User "Melman" "Fancypants" 0 False False
+  ]
+
+
+fetchUsers :: String -> Maybe [User]
+fetchUsers query =
+  case query of
+    "allusers" -> Just userList
+    "betausers" -> Just (filter beta userList)
+    "admins" -> Just (filter admin userList)
+    _ -> Nothing
+
+
+readSortMaybe :: String -> Maybe ([User] -> [User])
+readSortMaybe ordering =
+  case ordering of
+    "surname" -> Just (sortBy (compare `on` surname))
+    "age" -> Just (sortBy (compare `on` age))
+    _ -> Nothing
+
+
+liftAp :: Maybe (a -> b) -> Maybe a -> Maybe b
+liftAp _ Nothing = Nothing
+liftAp Nothing _ = Nothing
+liftAp (Just f) (Just x) = Just (f x)
+
+
+sortUsers :: String -> String -> Maybe [User]
+sortUsers ordering query =
+  liftAp
+  (readSortMaybe ordering)
+  (fetchUsers query)
