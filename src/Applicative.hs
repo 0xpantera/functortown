7232aa24eff864f391c6b136eff37ea919f2cc88
Francososa <franky.sosa@gmail.com>
2020-01-05 21:16:12 -0600

    Add database and greetUser function

--- src/Applicative.hs
+++ src/Applicative.hs
@@ -1,3 +1,20 @@
 module Applicative where
 
-x = 1
+
+database :: [(Integer, String)]
+database = [(1, "Mariela"),
+            (2, "Franky"),
+            (3, "Alonzo"),
+            (4, "Melman")]
+
+
+greetUser :: Integer -> Maybe String
+greetUser record =
+   mapToMaybe ("Hello, " ++) (lookup record database)
+
+
+mapToMaybe :: (String -> String) ->
+             (Maybe String -> Maybe String)
+mapToMaybe _ Nothing = Nothing
+mapToMaybe function (Just string) =
+  Just (function string)
