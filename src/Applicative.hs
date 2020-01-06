a4c7d4a266c15d0e05e42239821151c045f92d3c
Francososa <franky.sosa@gmail.com>
2020-01-05 21:24:56 -0600

    Generalize mapToMaybe

--- src/Applicative.hs
+++ src/Applicative.hs
@@ -13,8 +13,8 @@ greetUser record =
    mapToMaybe ("Hello, " ++) (lookup record database)
 
 
-mapToMaybe :: (String -> String) ->
-             (Maybe String -> Maybe String)
+mapToMaybe :: (a -> b) -> Maybe a -> Maybe b
 mapToMaybe _ Nothing = Nothing
-mapToMaybe function (Just string) =
-  Just (function string)
+mapToMaybe function (Just a) = Just (function a)
+
+
