81877900b527f1143bd73b9b7d835ba5fe03ba93
Francososa <franky.sosa@gmail.com>
2020-03-07 20:16:38 -0600

    Define data types and functor instances

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,19 @@
 module Lib where
 
-x = 2
+data Pair a = Pair a a deriving (Eq, Show)
+
+instance Functor Pair where
+  fmap f (Pair l r) = Pair (f l) (f r)
+
+
+data IncrementPair a =
+  IncrementPair Integer a deriving (Eq, Show)
+
+instance Functor IncrementPair where
+  fmap f (IncrementPair int r) = IncrementPair (int + 1) (f r)
+
+
+data BackwardPair a = BackwardPair a a deriving (Show, Eq)
+
+instance Functor BackwardPair where
+  fmap f (BackwardPair l r) = BackwardPair (f r) (f l)
