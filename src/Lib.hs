8923a8aed893259ee512542608ea8fb363912d37
Francososa <franky.sosa@gmail.com>
2020-03-13 11:59:36 -0600

    Define These data type with functor and bifunctor instances

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,17 @@
 module Lib where
 
+import Data.Bifunctor
+
 data Pair a = Pair a a deriving (Eq, Show)
 
 instance Functor Pair where
   fmap f (Pair l r) = Pair (f l) (f r)
+
+
+data BackwardPair a = BackwardPair a a deriving (Show, Eq)
+
+instance Functor BackwardPair where
+  fmap f (BackwardPair l r) = BackwardPair (f r) (f l)
 
 
 data IncrementPair a b =
@@ -12,8 +20,20 @@ data IncrementPair a b =
 instance Functor (IncrementPair a) where
   fmap f (IncrementPair l r) = IncrementPair l (f r)
 
+instance Bifunctor IncrementPair where
+  bimap f g (IncrementPair l r) =
+    IncrementPair (f l) (g r)
 
-data BackwardPair a = BackwardPair a a deriving (Show, Eq)
 
-instance Functor BackwardPair where
-  fmap f (BackwardPair l r) = BackwardPair (f r) (f l)
+data These a b = This a | That b | These a b
+  deriving Show
+
+instance Functor (These a) where
+  fmap _ (This x) = This x
+  fmap f (That y) = That (f y)
+  fmap f (These x y) = These x (f y)
+
+instance Bifunctor These where
+  bimap f _ (This x) = This (f x)
+  bimap _ g (That y) = That (g y)
+  bimap f g (These x y) = These (f x) (g y)
