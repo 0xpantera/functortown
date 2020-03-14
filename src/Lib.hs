64e7e7a0a46960ca33fe9b058830c692f5ce725c
Francososa <franky.sosa@gmail.com>
2020-03-13 23:55:17 -0600

    Define These datatype and add align instances to Either and Pair

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,9 @@
+{-# LANGUAGE InstanceSigs #-}
 module Lib where
 
 import Data.Bifunctor
-
-data Pair a = Pair a a deriving (Eq, Show)
-
-instance Functor Pair where
-  fmap f (Pair l r) = Pair (f l) (f r)
+import Data.Align
+import qualified Data.These as TH
 
 
 data BackwardPair a = BackwardPair a a deriving (Show, Eq)
@@ -25,15 +23,39 @@ instance Bifunctor IncrementPair where
     IncrementPair (f l) (g r)
 
 
-data These a b = This a | That b | These a b
+data These' a b = This' a | That' b | These' a b
   deriving Show
 
-instance Functor (These a) where
-  fmap _ (This x) = This x
-  fmap f (That y) = That (f y)
-  fmap f (These x y) = These x (f y)
+instance Functor (These' a) where
+  fmap _ (This' x) = This' x
+  fmap f (That' y) = That' (f y)
+  fmap f (These' x y) = These' x (f y)
 
-instance Bifunctor These where
-  bimap f _ (This x) = This (f x)
-  bimap _ g (That y) = That (g y)
-  bimap f g (These x y) = These (f x) (g y)
+instance Bifunctor These' where
+  bimap f _ (This' x) = This' (f x)
+  bimap _ g (That' y) = That' (g y)
+  bimap f g (These' x y) = These' (f x) (g y)
+
+
+instance Monoid a => Align (Either a) where
+  nil = Left mempty
+  align :: Either a b -> Either a c -> Either a (TH.These b c)
+  align (Left x) (Left x') = Left (x <> x')
+  align (Right y) (Left _) = Right (TH.This y)
+  align (Left _) (Right y) = Right (TH.That y)
+  align (Right y) (Right y') = Right (TH.These y y')
+
+
+data Pair a = Zero | Pair a a deriving (Eq, Show)
+
+instance Functor Pair where
+  fmap :: (a -> b) -> Pair a -> Pair b
+  fmap f (Pair l r) = Pair (f l) (f r)
+  fmap _ Zero = Zero
+
+instance Align Pair where
+  nil = Zero
+  align Zero Zero = Zero
+  align (Pair x y) Zero = Pair (TH.This x) (TH.This y)
+  align Zero (Pair x y) = Pair (TH.That x) (TH.That y)
+  align (Pair x y) (Pair x' y') = Pair (TH.These x x') (TH.These y y')
