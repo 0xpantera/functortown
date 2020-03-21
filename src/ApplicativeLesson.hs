4c1e8a1958666bb7d738937d1ae54cd24ba0516b
Francososa <franky.sosa@gmail.com>
2020-03-21 17:36:03 -0600

    Add applicative instances to option and either

--- src/ApplicativeLesson.hs
+++ src/ApplicativeLesson.hs
@@ -1,6 +1,45 @@
+{-# LANGUAGE InstanceSigs #-}
+import Control.Applicative
 import Data.List (sortBy)
 import Data.Function (on)
 import Numeric.Natural
+import Prelude hiding (Either (..))
+
+
+data Option a = Some a | None deriving (Eq, Show)
+
+instance Functor Option where
+  fmap _ None = None
+  fmap f (Some x) = Some (f x)
+
+instance Applicative Option where
+  pure :: a -> Option a
+  pure x = Some x
+  (<*>) :: Option (a -> b) -> Option a -> Option b
+  None <*> _ = None
+  _ <*> None = None
+  (Some f) <*> (Some x) = Some (f x)
+  liftA2 :: (a -> b -> c) -> Option a -> Option b -> Option c
+  liftA2 _ None None = None
+  liftA2 _ None _ = None
+  liftA2 _ _ None = None
+  liftA2 f (Some x) (Some y) = Some (f x y)
+
+data Either a b = Left a | Right b deriving (Eq, Show)
+
+instance Functor (Either a) where
+  fmap f (Left x) = Left x
+  fmap f (Right y) = Right (f y)
+
+instance Applicative (Either a) where
+  pure x = Right x
+  (Left x) <*> _ = Left x
+  _ <*> (Left y) = Left y
+  (Right f) <*> (Right y) = Right (f y)
+  liftA2 f (Left x) _ = Left x
+  liftA2 f _ (Left y) = Left y
+  liftA2 f (Right x) (Right y) = Right (f x y)
+
 
 
 data User = User { name :: String
@@ -23,19 +62,30 @@ userList = [
 
 fetchUsers :: String -> Maybe [User]
 fetchUsers query =
-    case query of
-        "allusers" -> Just userList
-        "betausers" -> Just (filter beta userList)
-        "admins" -> Just (filter admin userList)
-        _ -> Nothing
+  case query of
+    "allusers" -> Just userList
+    "betausers" -> Just (filter beta userList)
+    "admins" -> Just (filter admin userList)
+    _ -> Nothing
 
 
 readSortMaybe :: String -> Maybe ([User] -> [User])
 readSortMaybe ordering =
-    case ordering of
-        "surname" -> Just (sortBy (compare `on` surname))
-        "age" -> Just (sortBy (compare `on` age))
-        _ -> Nothing
+  case ordering of
+    "surname" -> Just (sortBy (compare `on` surname))
+    "age" -> Just (sortBy (compare `on` age))
+    _ -> Nothing
 
 
-        
+liftAp :: Maybe (a -> b) -> Maybe a -> Maybe b
+liftAp Nothing _ = Nothing
+liftAp _ Nothing = Nothing
+liftAp (Just f) (Just x) = Just (f x)
+
+
+sortUsers :: String -> String -> Maybe [User]
+sortUsers ordering query =
+  liftAp
+  (readSortMaybe ordering)
+  (fetchUsers query)
+
