04c3437a48e2cfc767667541e1bf0cc00dac810e
Francososa <franky.sosa@gmail.com>
2020-03-20 23:21:02 -0600

    Remove these library dependency by defining own type

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,6 @@ module Lib where
 
 import Data.Bifunctor
 import Data.Align
-import qualified Data.These as TH
 
 
 data BackwardPair a = BackwardPair a a deriving (Show, Eq)
@@ -23,27 +22,27 @@ instance Bifunctor IncrementPair where
     IncrementPair (f l) (g r)
 
 
-data These' a b = This' a | That' b | These' a b
+data These a b = This a | That b | These a b
   deriving Show
 
-instance Functor (These' a) where
-  fmap _ (This' x) = This' x
-  fmap f (That' y) = That' (f y)
-  fmap f (These' x y) = These' x (f y)
+instance Functor (These a) where
+  fmap _ (This x) = This x
+  fmap f (That y) = That (f y)
+  fmap f (These x y) = These x (f y)
 
-instance Bifunctor These' where
-  bimap f _ (This' x) = This' (f x)
-  bimap _ g (That' y) = That' (g y)
-  bimap f g (These' x y) = These' (f x) (g y)
+instance Bifunctor These where
+  bimap f _ (This x) = This (f x)
+  bimap _ g (That y) = That (g y)
+  bimap f g (These x y) = These (f x) (g y)
 
 
 instance Monoid a => Align (Either a) where
   nil = Left mempty
-  align :: Either a b -> Either a c -> Either a (TH.These b c)
+  align :: Either a b -> Either a c -> Either a (These b c)
   align (Left x) (Left x') = Left (x <> x')
-  align (Right y) (Left _) = Right (TH.This y)
-  align (Left _) (Right y) = Right (TH.That y)
-  align (Right y) (Right y') = Right (TH.These y y')
+  align (Right y) (Left _) = Right (This y)
+  align (Left _) (Right y) = Right (That y)
+  align (Right y) (Right y') = Right (These y y')
 
 
 data Pair a = Zero | Pair a a deriving (Eq, Show)
@@ -56,6 +55,6 @@ instance Functor Pair where
 instance Align Pair where
   nil = Zero
   align Zero Zero = Zero
-  align (Pair x y) Zero = Pair (TH.This x) (TH.This y)
-  align Zero (Pair x y) = Pair (TH.That x) (TH.That y)
-  align (Pair x y) (Pair x' y') = Pair (TH.These x x') (TH.These y y')
+  align (Pair x y) Zero = Pair (This x) (This y)
+  align Zero (Pair x y) = Pair (That x) (That y)
+  align (Pair x y) (Pair x' y') = Pair (These x x') (These y y')
