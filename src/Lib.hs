08cd5d8220a66bb03167f9bbf1b7b62e8c0a9d14
Francososa <franky.sosa@gmail.com>
2020-03-20 23:59:38 -0600

    Add first and second definition to These Bifunctor

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,7 @@
 module Lib where
 
 import Data.Bifunctor
-import Data.Align
+-- import Data.Align
 
 
 data BackwardPair a = BackwardPair a a deriving (Show, Eq)
@@ -23,7 +23,7 @@ instance Bifunctor IncrementPair where
 
 
 data These a b = This a | That b | These a b
-  deriving Show
+  deriving (Eq, Show)
 
 instance Functor (These a) where
   fmap _ (This x) = This x
@@ -31,11 +31,22 @@ instance Functor (These a) where
   fmap f (These x y) = These x (f y)
 
 instance Bifunctor These where
+  bimap :: (a -> b) -> (c -> d) -> (These a c) -> (These b d)
   bimap f _ (This x) = This (f x)
   bimap _ g (That y) = That (g y)
   bimap f g (These x y) = These (f x) (g y)
 
+  first :: (a -> b) -> These a c -> These b c
+  first f (This x) = This (f x)
+  first _ (That y) = That y
+  first f (These x y) = These (f x) y
+  second :: (c -> d) -> These a c -> These a d
+  
+  second _ (This x) = This x
+  second g (That y) = That (g y)
+  second g (These x y) = These x (g y)
 
+{-|
 instance Monoid a => Align (Either a) where
   nil = Left mempty
   align :: Either a b -> Either a c -> Either a (These b c)
@@ -43,7 +54,7 @@ instance Monoid a => Align (Either a) where
   align (Right y) (Left _) = Right (This y)
   align (Left _) (Right y) = Right (That y)
   align (Right y) (Right y') = Right (These y y')
-
+-}
 
 data Pair a = Zero | Pair a a deriving (Eq, Show)
 
@@ -52,9 +63,11 @@ instance Functor Pair where
   fmap f (Pair l r) = Pair (f l) (f r)
   fmap _ Zero = Zero
 
+{-|
 instance Align Pair where
   nil = Zero
   align Zero Zero = Zero
   align (Pair x y) Zero = Pair (This x) (This y)
   align Zero (Pair x y) = Pair (That x) (That y)
   align (Pair x y) (Pair x' y') = Pair (These x x') (These y y')
+-}
