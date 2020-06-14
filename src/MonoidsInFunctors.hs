a21b94d248a66d5e194dc746fc0b9f929478109e
Francososa <franky.sosa@gmail.com>
2020-06-14 08:05:44 -0600

    Define data type and first functions

--- src/MonoidsInFunctors.hs
+++ src/MonoidsInFunctors.hs
@@ -1,2 +1,22 @@
 module MonoidsInFunctors where
 
+import Control.Applicative
+import Data.Monoid
+
+data Person = Person {
+    name :: String
+  , pl   :: String 
+} deriving (Eq, Show)
+
+nonEmpty :: String -> Either String String
+nonEmpty str =
+    case null str of
+        True -> Left "Error: Empty string."
+        False -> Right str
+
+
+mkPerson :: String -> String -> Either String Person
+mkPerson name' pl' =  
+    Person <$> (nonEmpty name')
+           <*> (nonEmpty pl')
+
