9907a90ebad93df98279e6037e3714df267348fe
Francososa <franky.sosa@gmail.com>
2020-06-19 19:44:14 -0600

    Implement matching parenthesis parser

--- src/SequencingEffects.hs
+++ src/SequencingEffects.hs
@@ -1,1 +1,42 @@
+{-# LANGUAGE DeriveFunctor #-}
 module SequencingEffects where
+
+import Control.Applicative (Applicative (..))
+import qualified Data.List as List
+
+
+newtype Parser a = Parser (String -> Maybe (String, a))
+    deriving Functor
+
+instance Applicative Parser where
+    pure x = Parser (\str -> Just (str, x))
+    liftA2 = parserLiftA2
+
+parserLiftA2 :: (a -> b -> c)
+                -> Parser a -> Parser b
+                -> Parser c
+parserLiftA2 f (Parser p1) (Parser p2) =
+    Parser $ \str ->
+        do
+            (str', x) <- p1 str
+            (str'', y) <- p2 str'
+            Just (str'', f x y)
+
+parseMaybe :: Parser a -> String -> Maybe a
+-- String -> Maybe (String, p)
+parseMaybe (Parser p) str =
+    case (p str) of
+        Just ([], x) -> Just x
+        Just _ -> Nothing
+        Nothing -> Nothing
+
+exact :: String -> Parser String
+exact x = Parser $ \str ->
+    case (List.stripPrefix x str) of
+        Just str' -> Just (str', x)
+        Nothing -> Nothing
+
+anythingBut :: Char -> Parser String
+anythingBut c = Parser $ \str ->
+    let (match, remainder) = List.span (/= c) str
+    in Just (remainder, match)
